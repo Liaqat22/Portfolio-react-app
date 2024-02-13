e2e7d0bd903320681c0299a123750e42e7e8d858
Liaqat Ali <126480140+Liaqat22@users.noreply.github.com>
2024-02-13 13:16:09 +0500

    Update Data.js

--- src/ProjectData/Data.js
+++ src/ProjectData/Data.js
@@ -1,49 +1,61 @@
-import expenseMS from '../media/ExpenseMS_0.png'
-import ecomerce from '../media/ecommerce_1_0.png'
+// import expenseMS from '../media/ExpenseMS_0.png'
+// import ecomerce from '../media/ecommerce_1_0.png'
+// import dashboard from '../media/dashboard_0.png'
+// import jessy from '../media/jessy_walter_0.png'
+// import clothingstore from '../media/Capture_0.png'
+// import reacttutorialimg from '../media/reactTotorialimg_0.png'
 import simmon from '../media/simmon_0.png'
-import dashboard from '../media/dashboard_0.png'
-import jessy from '../media/jessy_walter_0.png'
 import pakrail from '../media/pakrail_0.png'
-import clothingstore from '../media/eCS_0.png'
-import reacttutorialimg from '../media/reactTotorialimg_0.png'
 
 export const projectContent = [
     {
      Projectname : 'Clothing Store',
-     ProjectImage : clothingstore,
+     ProjectImage : "https://firebasestorage.googleapis.com/v0/b/chatting-app-9ba60.appspot.com/o/upload%2Fimages%2FCapture_0.png?alt=media&token=3f739cba-22b1-4f59-932c-6d41c8f27c06",
      ProjectURL : 'https://clothing-store-ebon.vercel.app/',
-     ProjectDescription : ' A dynamic API-integrated system featuring protected admin and user panels, a secure payment gateway, customizable filters, a search bar, CRUD operations, and a streamlined shopping experience with a Cart Page.'
+     ProjectDescription : 'Api integration, Payment gatway, Cart page, filters, Searchbar, User/Admin protected routes,crud operations and many more . For USER experience e-mail : test@gmail.com , passworford : 333333 , For ADMIN experience e-mail : admin@gmail.com , password : 222222'
+    },
+    {
+     Projectname : 'Firebase Crud App',
+     ProjectImage : "https://firebasestorage.googleapis.com/v0/b/chatting-app-9ba60.appspot.com/o/upload%2Fimages%2Ffirebase_0.png?alt=media&token=e4b209a2-820c-4237-8067-3a343d8d1b11",
+     ProjectURL : 'https://firbase-react-crud-app.vercel.app/',
+     ProjectDescription : 'React with Firebase server, Firebase methods, user authentications, login with google, Signup page, add/edit/view/delete data, Responsive user interface . ; email : test@gmail.com / password : 123456'
+    },
+    {
+     Projectname : 'Firebase Chatting App',
+     ProjectImage : "https://firebasestorage.googleapis.com/v0/b/chatting-app-9ba60.appspot.com/o/upload%2Fimages%2FChatphoto_0.png?alt=media&token=6b99cfe7-8b46-45d6-b722-5f750ff2f2cc",
+     ProjectURL : 'https://firebase-react-chat-app-virid.vercel.app/',
+     ProjectDescription : "React with firebase server, Real time chat app, open the app on separated browser tabs and login with diffrent email and passwords given below.; person 1 = email : test@gmail.com, password:123456 .; person 2 : email : test2@gmail.com, password:123456"
     },
     {
      Projectname : 'Expense Managment System',
-     ProjectImage : expenseMS,
+     ProjectImage : "https://firebasestorage.googleapis.com/v0/b/chatting-app-9ba60.appspot.com/o/upload%2Fimages%2FExpenseMS_0.png?alt=media&token=2665a6cd-124a-4cb6-8fac-c7097f89a272",
      ProjectURL : 'https://lively-mite-gear.cyclic.app/',
-     ProjectDescription : 'Simplify expense management effortlessly with our dynamic API-integrated system. Enjoy easy transaction updates, additions, and deletions, along with robust filters and analytics. Experience a responsive user interface for enhanced usability.'
+     ProjectDescription : 'Api integration, User authentication, Add, Update, View, and Delete transection functionality, filters, Analytics, Responsive user interface.; e-mail : user@gmail.com , password : 123456'
     },
     {
      Projectname : 'E-commerce website',
-     ProjectImage : ecomerce,
+     ProjectImage : "https://firebasestorage.googleapis.com/v0/b/chatting-app-9ba60.appspot.com/o/upload%2Fimages%2Fecommerce_1_0.png?alt=media&token=10e79a44-3d61-4234-a082-bfaaf6df6390",
      ProjectURL : 'https://ecommercewebsitebyme.netlify.app/',
      ProjectDescription : 'Experience seamless online shopping on our cutting-edge React JS-powered ecommerce platform. Explore a diverse product range, navigate effortlessly with a user-friendly interface, and shop securely. Elevate your journey with responsive design and dynamic features for ultimate satisfaction.'
     },
     {
      
      Projectname : 'Intern dashboard',
-     ProjectImage : dashboard,
+     ProjectImage : "https://firebasestorage.googleapis.com/v0/b/chatting-app-9ba60.appspot.com/o/upload%2Fimages%2Fdashboard_0.png?alt=media&token=a2989904-e9f6-4958-8a86-401da801b8ad",
      ProjectURL : 'https://employedashboard.netlify.app',
       ProjectDescription :"Discover the future of workforce management with our cutting-edge Intern Dashboard! Crafted in React, it offers a fully responsive, attractive UI, intuitive controls, and seamless task management. Elevate your team's performance and efficiency today!"
     },
     {
  
      Projectname : 'Jessy portfolio',
-     ProjectImage : jessy,
+     ProjectImage : "https://firebasestorage.googleapis.com/v0/b/chatting-app-9ba60.appspot.com/o/upload%2Fimages%2Fjessy_walter_0.png?alt=media&token=44c71ebb-d852-42bc-8fab-95668adf7ea1",
      ProjectURL : 'https://jessywalter-portfolio.netlify.app',
       ProjectDescription :"Discover the enchanting world of my fully responsive and captivating portfolio website, created with React. A single-page application that showcases my projects with seamless navigation and delightful animations. Let's collaborate and turn your vision into a digital masterpiece. Hire me now and let creativity and innovation lead the way!"
     },
     {
     
       Projectname : 'React Tutorial App',
-      ProjectImage : reacttutorialimg,
+      ProjectImage : "https://firebasestorage.googleapis.com/v0/b/chatting-app-9ba60.appspot.com/o/upload%2Fimages%2FreactTotorialimg_0.png?alt=media&token=3fd706c0-7d63-4724-85a4-4260d0ad20ad",
       ProjectURL : 'https://react-tutorial-app-one.vercel.app/',
        ProjectDescription :"The React tutorial app immerses users in fundamental concepts like hooks, lifecycle methods, and function/class-based components. Through hands-on modules, it facilitates a comprehensive understanding of React's core principles for robust UI development."
      }, 
