89907fce1e7f4ba9a6dc683ac15979ed56eed454
Liaqat Ali <126480140+Liaqat22@users.noreply.github.com>
2024-02-17 00:09:46 +0500

    Update Data.js

--- src/ProjectData/Data.js
+++ src/ProjectData/Data.js
@@ -14,12 +14,7 @@ export const projectContent = [
      ProjectURL : 'https://clothing-store-ebon.vercel.app/',
      ProjectDescription : 'Api integration, Payment gatway, Cart page, filters, Searchbar, User/Admin protected routes,crud operations and many more . For USER experience e-mail : test@gmail.com , passworford : 333333 , For ADMIN experience e-mail : admin@gmail.com , password : 222222'
     },
-    {
-     Projectname : 'Firebase Crud App',
-     ProjectImage : "https://firebasestorage.googleapis.com/v0/b/chatting-app-9ba60.appspot.com/o/upload%2Fimages%2Ffirebase_0.png?alt=media&token=e4b209a2-820c-4237-8067-3a343d8d1b11",
-     ProjectURL : 'https://firbase-react-crud-app.vercel.app/',
-     ProjectDescription : 'React with Firebase server, Firebase methods, user authentications, login with google, Signup page, add/edit/view/delete data, Responsive user interface . ; email : test@gmail.com / password : 123456'
-    },
+  
     {
      Projectname : 'Firebase Chatting App',
      ProjectImage : "https://firebasestorage.googleapis.com/v0/b/chatting-app-9ba60.appspot.com/o/upload%2Fimages%2FChatphoto_0.png?alt=media&token=6b99cfe7-8b46-45d6-b722-5f750ff2f2cc",
@@ -27,17 +22,25 @@ export const projectContent = [
      ProjectDescription : "React with firebase server, Real time chat app, open the app on separated browser tabs and login with diffrent email and passwords given below.; person 1 = email : test@gmail.com, password:123456 .; person 2 : email : test2@gmail.com, password:123456"
     },
     {
-     Projectname : 'Expense Managment System',
-     ProjectImage : "https://firebasestorage.googleapis.com/v0/b/chatting-app-9ba60.appspot.com/o/upload%2Fimages%2FExpenseMS_0.png?alt=media&token=2665a6cd-124a-4cb6-8fac-c7097f89a272",
-     ProjectURL : 'https://lively-mite-gear.cyclic.app/',
-     ProjectDescription : 'Api integration, User authentication, Add, Update, View, and Delete transection functionality, filters, Analytics, Responsive user interface.; e-mail : user@gmail.com , password : 123456'
-    },
+      Projectname : 'Expense Managment System',
+      ProjectImage : "https://firebasestorage.googleapis.com/v0/b/chatting-app-9ba60.appspot.com/o/upload%2Fimages%2FExpenseMS_0.png?alt=media&token=2665a6cd-124a-4cb6-8fac-c7097f89a272",
+      ProjectURL : 'https://lively-mite-gear.cyclic.app/',
+      ProjectDescription : 'Api integration, User authentication, Add, Update, View, and Delete transection functionality, filters, Analytics, Responsive user interface.; e-mail : user@gmail.com , password : 123456'
+     },
     {
-     Projectname : 'E-commerce website',
-     ProjectImage : "https://firebasestorage.googleapis.com/v0/b/chatting-app-9ba60.appspot.com/o/upload%2Fimages%2Fecommerce_1_0.png?alt=media&token=10e79a44-3d61-4234-a082-bfaaf6df6390",
-     ProjectURL : 'https://ecommercewebsitebyme.netlify.app/',
-     ProjectDescription : 'Experience seamless online shopping on our cutting-edge React JS-powered ecommerce platform. Explore a diverse product range, navigate effortlessly with a user-friendly interface, and shop securely. Elevate your journey with responsive design and dynamic features for ultimate satisfaction.'
-    },
+      Projectname : 'Tech-store',
+      ProjectImage : "https://firebasestorage.googleapis.com/v0/b/chatting-app-9ba60.appspot.com/o/upload%2Fimages%2Fecommerce_1_0.png?alt=media&token=10e79a44-3d61-4234-a082-bfaaf6df6390",
+      ProjectURL : 'https://ecommercewebsitebyme.netlify.app/',
+      ProjectDescription : 'Experience seamless online shopping on our cutting-edge React JS-powered ecommerce platform. Explore a diverse product range, navigate effortlessly with a user-friendly interface, and shop securely. Elevate your journey with responsive design and dynamic features for ultimate satisfaction.'
+     },
+     {
+      Projectname : 'Firebase Crud App',
+      ProjectImage : "https://firebasestorage.googleapis.com/v0/b/chatting-app-9ba60.appspot.com/o/upload%2Fimages%2Ffirebase_0.png?alt=media&token=e4b209a2-820c-4237-8067-3a343d8d1b11",
+      ProjectURL : 'https://firbase-react-crud-app.vercel.app/',
+      ProjectDescription : 'React with Firebase server, Firebase methods, user authentications, login with google, Signup page, add/edit/view/delete data, Responsive user interface . ; email : test@gmail.com / password : 123456'
+     },
+   
+   
     {
      
      Projectname : 'Intern dashboard',
