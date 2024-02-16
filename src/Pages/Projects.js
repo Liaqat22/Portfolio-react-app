23ece10296019ff1f4c446c4a8e60c245afb0b0c
Liaqat Ali <126480140+Liaqat22@users.noreply.github.com>
2024-02-17 00:13:24 +0500

    Update Projects.js

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -40,14 +40,21 @@ function Projects() {
         projectContent.slice(0,3).map((project, i) => (
           <div className='container-fluid containerfluid' style={{ overflow: 'hidden' }}>
             <div className='row' key={project._id}>
-              <div className='col-md-6'>
                 <h1 className='headings' >- Project {i + 1}</h1>
+              <div className='col-md-6'>
                 <h1 className='headingxl ' style={{color:"#f8f7ff"}}>{project.Projectname}</h1>
                 <p className='paratext ' data-aos="fade-right">
                   {project.ProjectDescription}
                 </p>
 
-                <div className='btun m-3' data-aos="fade-left"><i className="fa-solid fa-arrow-right" style={{ color: 'yellow'}} />
+               
+
+
+              </div>
+              <div className='col-md-6' data-aos="fade-left">
+                <img className="imgproject" src={project.ProjectImage} alt='.' />
+              </div>
+              <div className='btun m-3 p-3' data-aos="fade-left"><i className="fa-solid fa-arrow-right" style={{ color: 'yellow'}} />
                   <NavLink
                     className='btn btn-secondary projectbtn'
                     to={project.ProjectURL}
@@ -58,12 +65,6 @@ function Projects() {
                     Click to explore
                   </NavLink>   
                 </div>
-
-
-              </div>
-              <div className='col-md-6' data-aos="fade-left">
-                <img className="imgproject" src={project.ProjectImage} alt='.' />
-              </div>
             </div>
           </div>
         ))
