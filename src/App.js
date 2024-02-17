63a0a4e2262abd1afebc3b6a166d9a4750d1b33d
Liaqat Ali <126480140+Liaqat22@users.noreply.github.com>
2024-02-17 19:01:49 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import './App.css';
 import Layout from './Layout/Layout';
-import Home from './Pages/Home';
-import {Routes , Route} from 'react-router-dom'
-import Portfolio from './Pages/Portfolio';
-import Contacts from './Pages/Contacts';
+import { Routes, Route } from 'react-router-dom';
+import { lazy,Suspense } from 'react';
+
+const Home = lazy(() => import('./Pages/Home'));
+const Portfolio = lazy(() => import('./Pages/Portfolio'));
+const Contacts = lazy(() => import('./Pages/Contacts'));
 
 function App() {
   return (
-    <div >
-    <Layout>
-      <Routes>
-        <Route path = '/' element ={<Home/>}/>
-        <Route path = '/portfolio' element ={<Portfolio/>}/>
-        <Route path = '/contact' element ={<Contacts/>}/>
-      </Routes>
-    </Layout>
-     
+    <div>
+      <Layout>
+        <Suspense >
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/portfolio" element={<Portfolio />} />
+            <Route path="/contact" element={<Contacts />} />
+          </Routes>
+        </Suspense>
+      </Layout>
     </div>
   );
 }
 
 export default App;
+ 
