033575e8fc067d9968788cab411678114c7073c0
Liaqat Ali <126480140+Liaqat22@users.noreply.github.com>
2024-03-02 22:20:35 +0500

    Update Skills.js

--- src/Pages/Skills.js
+++ src/Pages/Skills.js
@@ -8,6 +8,7 @@ import firebase from "../media/icons/firebase.png"
 import bootstrap from "../media/icons/pngegg.png"
 import github from "../media/icons/github.png"
 import materialUI from "../media/icons/pngwing.com.png"
+import redux from "../media/icons/redux.png"
 
 function Skills() {
     const skills = [
@@ -16,6 +17,7 @@ function Skills() {
         { skillImg : html,skillName : "html"},
         { skillImg :css,skillName : "css"},
         { skillImg : js,skillName : "js"},
+        { skillImg : redux,skillName : "redux"},
         { skillImg : bootstrap,skillName : "bootstrap"},
         { skillImg : materialUI,skillName : "materialUI"},
         { skillImg : github,skillName : "github"},
