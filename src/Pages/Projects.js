60a6323a1211a5021ec09c0de1be33e9d2984de5
Liaqat Ali <126480140+Liaqat22@users.noreply.github.com>
2024-07-05 08:33:45 +0500

    Update Projects.js

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -1,44 +1,18 @@
 import React from 'react'
-// import axios from 'axios'
-// import { message, Spin } from 'antd';
 import { NavLink } from 'react-router-dom'
 import { projectContent } from '../ProjectData/Data';
 
 
 function Projects() {
-    // const [ loading , setLoadng] = useState(false)
-  //   const [projects , setProjects] = useState([])
-
-  //   //all projects
-  
-  // const getAllProjects = async () => {
-  //   try {
-  //     setLoadng(true)
-  //     const { data } =await axios.get(
-  //       `https://personal-portfolio-api-s.vercel.app/api/v1/project/get-project`);
-  //     if (data?.success) {
-  //       setProjects(data?.projects)
-  //       setLoadng(false)
-  //              console.log("project got Successfully");   
- 
-  //     }
-  //   } catch (error) {
-  //     console.log(error);
-  //     message.error("something went wrong");
-  //     setLoadng(false)
-  //   }}
-  //   useEffect(()=>{
-  //     getAllProjects()
-  //   },[])
+   
   return (
     <>
       <div className='text-center '>
      <h1 className='text-center headingxl services' data-aos="fade-down" >Latest Projects</h1>
-       {/* {loading&&  <Spin tip="Loading" size="large"/>} */}
       </div>
+      <div className='container-fluid containerfluid' style={{ overflow: 'hidden' }}>
       {
         projectContent.slice(0,3).map((project, i) => (
-          <div className='container-fluid containerfluid' style={{ overflow: 'hidden' }}>
             <div className='row' key={project._id}>
                 <h1 className='headings' >- Project {i + 1}</h1>
               <div className='col-md-6'>
@@ -66,9 +40,9 @@ function Projects() {
                   </NavLink>   
                 </div>
             </div>
-          </div>
         ))
       }
+          </div>
 
 
       <div className='d-flex justify-content-center pb-3 '>
