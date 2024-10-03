fbfb025a61f28f088a31dc9a482a55483f8f3c9f
Liaqat Ali <126480140+Liaqat22@users.noreply.github.com>
2024-10-03 21:43:21 +0500

    Update Footer.js

--- src/Layout/Footer.js
+++ src/Layout/Footer.js
@@ -6,10 +6,10 @@ function Footer() {
     <div>
         <div className='container-fluid px-0 mt-3'>
           <div className='row bg-dark p-3 m-0 d-flex align-items-center'>
-            <div className='col-md-9'>
+            <div className='col-md-8'>
             <Typography variant='h6' className=' text-white footer'>All Rights are Reserved &copy; Liaqat Ali 2024</Typography>
             </div>
-            <div className='col-md-3' style={{display:'flex',justifyContent:'space-evenly'}}>
+            <div className='col-md-4' style={{display:'flex',justifyContent:'space-evenly'}}>
            <a href='https://liaqatali097306@gmail.com'><i className="fa-solid envelope fa-envelope p-0" style={{color:' #61DBFB',filter:"none"}}/></a> 
            <a href='https://www.linkedin.com/in/liaqat-ali-bb086522a/'> <i className="fa-brands fa-linkedin" style={{color:' #61DBFB'}}/></a>
            <a href='https://wa.me/qr/FTMDCAW2J6XEP1'><i className="fa-brands fa-whatsapp" style={{color:' #61DBFB'}}/></a> 
